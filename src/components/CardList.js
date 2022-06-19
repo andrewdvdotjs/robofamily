f00cc3d1bd165f764b6b910728e54cbb08a19f77
Andre <andre.duarte.vieira06@gmail.com>
2022-06-19 18:49:50 -0300

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from "./Card";
+
+
+const CardList = ({ robots }) =>{
+    const cardComponent = robots.map((user, i) => {
+        return (<Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+        />
+        );
+    })
+    return (
+        <div>
+            {cardComponent}
+        </div>
+    );
+}
+
+export default CardList;
+
+/*
+* 'é melhor fazemos loop pois caso adicionemos muitos robôs,
+* a gnt n precisa ficar dando ctrl c ctrl v toda hora
+* */
